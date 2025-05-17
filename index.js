69ed53319d5439ab576e9af3186a1ce0595f3e90
Khalo17 <99505845+Khalo17@users.noreply.github.com>
2025-05-17 05:21:22 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+/*let form = document.getElementById('form')
+let button = document.getElementById('buttons')
+let mobile = document.getElementById('mobile')
+let password = document.getElementById('password')
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let name = mobile.value
+    let passwor = password.value
+    console.log(name,passwor)
+    //
+    //await fetch("http://localhost:3002/")
+    //json = await resp.json()
+    //console.log(json)
+    //async function fun(){
+        const resp = await fetch('http://localhost:3002/',{
+        method:"POST",
+        body:JSON.stringify({username:name,userpass:passwor})
+        })
+        let json = await resp.json()
+        console.log(json)
+    }
+    fun()
+})*/
+alert('khaled')
